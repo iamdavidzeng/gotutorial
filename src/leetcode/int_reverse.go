741dbf5bf8d0b3b3be5ad870f12c32029e067f9e
zzy <15779712170@163.com>
2019-08-18 17:13:43 +0800

    Go implements intReverse

---
+++ src/leetcode/int_reverse.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+
+func intReverse(target int) int {
+	y := 0
+	for target != 0 {
+		y = y * 10 + target % 10
+		if !( -(1<<31) <= y && y <= (1<<31) -1 ) {
+			return 0
+		}
+		target /= 10
+	}
+	return y
+}
+
+
+func main() {
+
+	fmt.Print(intReverse(321))
+}
