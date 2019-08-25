069034a60955bca61adf4fe609410465d9296314
zzy <15779712170@163.com>
2019-08-25 14:46:35 +0800

    is_happy

---
+++ src/leetcode/is_happy.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+
+func isHappy(n int) bool {
+	history := make(map[int] bool)
+
+	for n != 1 {
+		if _, ok := history[n]; ok {
+			return false
+		}
+		history[n] = true
+
+		sumValue := 0
+		for n != 0 {
+			sumValue += (n % 10) * (n % 10)
+			n /= 10
+		}
+		
+		n = sumValue
+	}
+	return true
+}
+
+
+func main() {
+	fmt.Println(isHappy(19))
+}
