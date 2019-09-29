0641a37a5adbc2b995cd63cb5bade9916c444206
zzy <15779712170@163.com>
2019-09-29 23:42:56 +0800

    TARA-1 Be familiar with basic

--- src/go_db/initial_db.go
+++ src/go_db/initial_db.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open(
-		"mysql", "root:@tcp(127.0.0.1:3306)/users"
-	)
+	fmt.Println("Go MySQL Tutorial")
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/users")
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
 	}
+
 	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM users.users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer results.Close()
 }
---
+++ src/go_db/populating_structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Tag struct {
+	ID    int    `json: "id"`
+	Email string `json: "email"`
+}
+
+func main() {
+
+	fmt.Println("Go MYSQL Tutorial")
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	// Execute the query
+	results, err := db.Query("SELECT id, email FROM users.users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var tag Tag
+		err = results.Scan(&tag.ID, &tag.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println(tag.Email)
+	}
+}
