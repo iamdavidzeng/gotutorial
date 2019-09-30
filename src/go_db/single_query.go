8c298c63ab1c114c1b32452e0b73943e55ae786d
zzy <15779712170@163.com>
2019-09-30 19:37:03 +0800

    TARA-1 Single query

---
+++ src/go_db/single_query.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Tag struct {
+	ID    int
+	Email string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	var tag Tag
+	err = db.QueryRow("SELECT id, email FROM users.users WHERE id=?", 2).Scan(&tag.ID, &tag.Email)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println(tag.ID)
+	fmt.Println(tag.Email)
+}
