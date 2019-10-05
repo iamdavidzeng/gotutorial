31869c2ba47a53aa2eb76c1b569dbb69998b400b
zzy <15779712170@163.com>
2019-10-05 22:37:03 +0800

    ZARA-1 Correct todo apis

--- src/tutorial/todo_api.go
+++ src/tutorial/todo_api.go
@@ -11,27 +11,19 @@ import (
 
 var db *gorm.DB
 
-type (
-	todoModel struct {
-		gorm.Model
-		Title     string `json:"title"`
-		Completed int    `json:"completed"`
+func init() {
+	//创建一个数据库的连接
+	var err error
+	db, err = gorm.Open("mysql", "root:@/demo?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
 	}
 
-	transformedTodo struct {
-		ID        uint   `json:"id"`
-		Title     string `json:"title"`
-		Completed bool   `json:completed`
-	}
-)
+	//迁移the schema
+	db.AutoMigrate(&todoModel{})
+}
 
 func main() {
-
-	db, err := gorm.Open("mysql", "root:@/demo")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
 
 	router := gin.Default()
 
@@ -44,22 +36,34 @@ func main() {
 		v1.DELETE("/:id", deleteTodo)
 	}
 	router.Run()
+
 }
 
+type (
+	// todoModel 包括了 todoModel 的字段类型
+	todoModel struct {
+		gorm.Model
+		Title     string `json:"title"`
+		Completed int    `json:"completed"`
+	}
+
+	// transformedTodo 代表格式化的 todo 结构体
+	transformedTodo struct {
+		ID        uint   `json:"id"`
+		Title     string `json:"title"`
+		Completed bool   `json:"completed"`
+	}
+)
+
+// createTodo 方法添加一条新的 todo 数据
 func createTodo(c *gin.Context) {
 	completed, _ := strconv.Atoi(c.PostForm("completed"))
 	todo := todoModel{Title: c.PostForm("title"), Completed: completed}
 	db.Save(&todo)
-	c.JSON(
-		http.StatusCreated,
-		gin.H{
-			"status":     http.StatusCreated,
-			"message":    "Todo item created successfully!",
-			"resourceId": todo.ID,
-		},
-	)
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "resourceId": todo.ID})
 }
 
+// fetchAllTodo 返回所有的 todo 数据
 func fetchAllTodo(c *gin.Context) {
 	var todos []todoModel
 	var _todos []transformedTodo
@@ -67,13 +71,11 @@ func fetchAllTodo(c *gin.Context) {
 	db.Find(&todos)
 
 	if len(todos) <= 0 {
-		c.JSON(
-			http.StatusNotFound,
-			gin.H{"status": http.StatusNotFound, "message": "No todo found!"},
-		)
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
 
+	//转化 todos 数据，用来格式化
 	for _, item := range todos {
 		completed := false
 		if item.Completed == 1 {
@@ -81,14 +83,12 @@ func fetchAllTodo(c *gin.Context) {
 		} else {
 			completed = false
 		}
-		_todos = append(
-			_todos,
-			transformedTodo{ID: item.ID, Title: item.Title, Completed: completed},
-		)
+		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, Completed: completed})
 	}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
 }
 
+// fetchSingleTodo方法返回一条 todo 数据
 func fetchSingleTodo(c *gin.Context) {
 	var todo todoModel
 	todoID := c.Param("id")
@@ -96,10 +96,7 @@ func fetchSingleTodo(c *gin.Context) {
 	db.First(&todo, todoID)
 
 	if todo.ID == 0 {
-		c.JSON(
-			http.StatusNotFound,
-			gin.H{"status": http.StatusNotFound, "message": "No todo found!"},
-		)
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
 
@@ -114,6 +111,7 @@ func fetchSingleTodo(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todo})
 }
 
+// updateTodo 方法 更新 todo 数据
 func updateTodo(c *gin.Context) {
 	var todo todoModel
 	todoID := c.Param("id")
@@ -121,10 +119,7 @@ func updateTodo(c *gin.Context) {
 	db.First(&todo, todoID)
 
 	if todo.ID == 0 {
-		c.JSON(
-			http.StatusNotFound,
-			gin.H{"status": http.StatusNotFound, "message": "No todo found!"},
-		)
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
 
@@ -134,6 +129,7 @@ func updateTodo(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo updated successfully!"})
 }
 
+// deleteTodo 方法依据 id 删除一条todo 数据
 func deleteTodo(c *gin.Context) {
 	var todo todoModel
 	todoID := c.Param("id")
@@ -141,17 +137,10 @@ func deleteTodo(c *gin.Context) {
 	db.First(&todo, todoID)
 
 	if todo.ID == 0 {
-		c.JSON(
-			http.StatusNotFound,
-			gin.H{"status": http.StatusNotFound, "message": "No todo found"},
-		)
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
 		return
 	}
 
 	db.Delete(&todo)
-
-	c.JSON(
-		http.StatusOK,
-		gin.H{"status": http.StatusOK, "message": "Todo deleted successfully!"},
-	)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo deleted successfully!"})
 }
