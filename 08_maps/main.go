6085b7071791a68efb1b26c8be06b19c4d93a17b
zzy <15779712170@163.com>
2020-01-01 15:05:37 +0800

    maps catch up

---
+++ 08_maps/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Use make declare map
+	// emails := make(map[string]string)
+
+	// emails["David"] = "david@gmail.com"
+	// emails["Bob"] = "bob@gmail.com"
+
+	// Declare map and assign value
+	emails := map[string]string{"David": "david.gmail.com", "Bob": "bob@gmail.com"}
+	fmt.Println(emails)
+}
