54c6b0a6fc5a5a7f2baaeff52307c705f8bb1bf2
iamdavidzeng <15779712170@163.com>
2020-04-22 15:18:00 +0800

    first step

--- leetcode/utils/max_profit.go
+++ leetcode/utils/max_profit.go
@@ -3,7 +3,24 @@ package main
 import "fmt"
 
 func maxProfit(prices []int) int {
-	return 1
+	min_price_index := 0
+	max_price_index := 0
+	profit := 0
+	for i := 0; i < len(prices)-1; i++ {
+		if prices[i] >= prices[i+1] {
+			min_price_index = i + 1
+		} else {
+			max_price_index = i + 1
+		}
+
+		if min_price_index == len(prices)-1 {
+			return 0
+		} else if max_price_index == len(prices)-1 {
+			return prices[max_price_index] - prices[min_price_index]
+		}
+	}
+
+	return profit
 }
 
 func main() {
