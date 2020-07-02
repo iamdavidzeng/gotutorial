12c2af2551e8c11c8809f418b91fb82dcf06111a
iamdavidzeng <15779712170@163.com>
2020-07-02 18:55:45 +0800

    Fix lint

--- stripe/seperate_transfers.go
+++ stripe/seperate_transfers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/paymentintent"
 	"github.com/stripe/stripe-go/v71/transfer"
@@ -11,26 +13,26 @@ func main() {
 	stripe.Key = os.Getenv("STRIPE_ACCOUNT_SECRET_KEY")
 
 	params := &stripe.PaymentIntentParams{
-		Amount: stripe.Int64(10000),
-		Currency: stripe.String(string(stripe.CurrencyGBP)),
-		PaymentMethodTypes: stripe.StringSlice([]string{"card",}),
+		Amount:             stripe.Int64(10000),
+		Currency:           stripe.String(string(stripe.CurrencyGBP)),
+		PaymentMethodTypes: stripe.StringSlice([]string{"card"}),
 	}
 	pi, _ := paymentintent.New(params)
 
 	// Create first Transfer
 	transferParams := &stripe.TransferParams{
-		Amount: stripe.Int64(7000),
-		Currency: stripe.String(string(stripe.CurrencyGBP)),
-		Destination: stripe.String(os.Getenv("FIRST_CONNECTED_ACCOUNT_ID")),
+		Amount:        stripe.Int64(7000),
+		Currency:      stripe.String(string(stripe.CurrencyGBP)),
+		Destination:   stripe.String(os.Getenv("FIRST_CONNECTED_ACCOUNT_ID")),
 		TransferGroup: stripe.String(*&pi.TransferGroup),
 	}
 	tr1, _ := transfer.New(transferParams)
 
 	// Create second Transfer
 	secondTransferParams := &stripe.TransferParams{
-		Amount: stripe.Int64(2000),
-		Currency: stripe.String(string(stripe.CurrencyGBP)),
-		Destination: stripe.String(os.Getenv("SECOND_CONNECTED_ACCOUNT_ID")),
+		Amount:        stripe.Int64(2000),
+		Currency:      stripe.String(string(stripe.CurrencyGBP)),
+		Destination:   stripe.String(os.Getenv("SECOND_CONNECTED_ACCOUNT_ID")),
 		TransferGroup: stripe.String(*&pi.TransferGroup),
 	}
 	tr2, _ := transfer.New(secondTransferParams)
