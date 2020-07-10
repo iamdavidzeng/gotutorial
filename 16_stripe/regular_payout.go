b33b003458e9c35c7678efe3740e0a852a15f8b1
iamdavidzeng <15779712170@163.com>
2020-07-10 15:49:27 +0800

    Add new param for Payout

--- 16_stripe/regular_payout.go
+++ 16_stripe/regular_payout.go
@@ -16,7 +16,11 @@ func main() {
 	params := &stripe.PayoutParams{
 		Amount: stripe.Int64(100),
 		Currency: stripe.String(string(stripe.CurrencyGBP)),
+		// Specify Destination to Payout, Otherwise use default external account instead
+		// Destination: stripe.String(os.Getenv("BANK_ACCOUNT")),
 	}
+	// Payout for Connected Account
+	// params.SetStripeAccount(os.Getenv("FIRST_CONNECTED_ACCOUNT_ID"))
 
 	po, _ := payout.New(params)
 	
