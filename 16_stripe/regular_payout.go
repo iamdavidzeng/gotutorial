c8c3dc3b08b680e5bab5c37786838a571792fc71
iamdavidzeng <15779712170@163.com>
2020-07-30 16:13:25 +0800

    Build go_stripe_account

--- 16_stripe/regular_payout.go
+++ 16_stripe/regular_payout.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 
 	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/payout"
 )
-
 
 func main() {
 
 	stripe.Key = os.Getenv("STRIPE_ACCOUNT_SECRET_KEY")
 
 	params := &stripe.PayoutParams{
-		Amount: stripe.Int64(100),
+		Amount:   stripe.Int64(100),
 		Currency: stripe.String(string(stripe.CurrencyGBP)),
 		// Specify Destination to Payout, Otherwise use default external account instead
 		// Destination: stripe.String(os.Getenv("BANK_ACCOUNT")),
@@ -23,6 +22,6 @@ func main() {
 	// params.SetStripeAccount(os.Getenv("FIRST_CONNECTED_ACCOUNT_ID"))
 
 	po, _ := payout.New(params)
-	
+
 	fmt.Println(po)
 }
