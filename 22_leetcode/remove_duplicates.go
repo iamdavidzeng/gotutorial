4e512bf7ddf4215c9221f48d2943d3cef41739a7
zzy <15779712170@163.com>
2020-08-30 16:45:08 +0800

    Rename dic

--- 22_leetcode/remove_duplicates.go
+++ 22_leetcode/remove_duplicates.go
@@ -5,17 +5,13 @@ import "fmt"
 func removeDuplicates(nums []int) int {
 
 	for i := 1; i < len(nums); i++ {
-
 		for j := 0; j < i; j++ {
-
 			if nums[i] == nums[j] {
 				nums = append(nums[:i], nums[i+1:]...)
 				i--
 			}
 		}
 	}
-
-	fmt.Println(nums)
 
 	return len(nums)
 }
@@ -24,6 +20,6 @@ func main() {
 
 	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 
-	removeDuplicates(nums)
+	fmt.Println(removeDuplicates(nums))
 
 }
