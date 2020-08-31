8c7d0e54fa21a58f5530d0987710bd0a336368ec
zzy <15779712170@163.com>
2020-08-31 23:30:09 +0800

    rotate

--- 22_leetcode/rotate.go
+++ 22_leetcode/rotate.go
@@ -1,5 +1,15 @@
 package main
 
-func rotate(nums []int, k int) {
+import "fmt"
 
+func rotate(nums []int, k int) []int {
+	i := len(nums) - k
+	nums = append(nums[i:], nums[:i]...)
+	return nums
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+
+	fmt.Println(rotate(nums, 3))
 }
