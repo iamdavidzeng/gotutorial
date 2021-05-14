178d9ec952e08884b331f942a4237508e695fc4e
iamdavidzeng <15779712170@163.com>
2021-05-14 11:10:16 +0800

    Update load config by regex

--- 19_load_config/load_by_viper.go
+++ 19_load_config/load_by_viper.go
@@ -8,11 +8,8 @@ import (
 	"github.com/spf13/viper"
 )
 
-type DBCfg struct {
-	DBURI    string `mapstructure:"DB_URI"`
-	DBServer struct {
-		URI string `mapstructure:"URI"`
-	} `mapstructure:"DB"`
+type DatabaseCfg struct {
+	Address string `mapstructure:"DB_URL"`
 }
 
 func main() {
@@ -26,13 +23,12 @@ func main() {
 	}
 
 	for _, key := range viper.AllKeys() {
-		fmt.Println(key)
 		value := viper.GetString(key)
-		envOrRaw := replaceEnvInConfig([]byte(value))
-		viper.Set(key, string(envOrRaw))
+		envOrRaw := replace(value)
+		viper.Set(key, envOrRaw)
 	}
 
-	var config DBCfg
+	var config DatabaseCfg
 	if err := viper.Unmarshal(&config); err != nil {
 		panic(fmt.Errorf("failed to load"))
 	}
@@ -40,23 +36,21 @@ func main() {
 	fmt.Println(config)
 }
 
-func replace(body []byte) []byte {
+func replace(s string) string {
 	compiler := regexp.MustCompile(`\$\{([^}:]+):?([^}]+)?\}`)
+	value := compiler.ReplaceAllFunc([]byte(s), func(b []byte) []byte {
 
-}
+		match := compiler.FindStringSubmatch(string(b))
+		fmt.Println(match)
 
-func replaceEnvInConfig(body []byte) []byte {
-	search := regexp.MustCompile(`\$\{([^}:]+):?([^}]+)?\}`)
-	replacedBody := search.ReplaceAllFunc(body, func(b []byte) []byte {
-		group1 := search.ReplaceAllString(string(b), `$1`)
-		group2 := search.ReplaceAllString(string(b), `$2`)
+		envValue := os.Getenv(match[1])
+		defaultValue := match[2]
 
-		envValue := os.Getenv(group1)
-		if len(envValue) > 0 {
+		if len([]byte(envValue)) > 0 {
 			return []byte(envValue)
 		}
-		return []byte(group2)
+		return []byte(defaultValue)
 	})
 
-	return replacedBody
+	return string(value)
 }
