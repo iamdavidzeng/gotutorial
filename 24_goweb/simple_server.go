ac979b32927af97b6b514a645f740291adb36cc4
iamdavidzeng <15779712170@163.com>
2021-12-03 11:46:46 +0800

    chore(websocket): implements two users chat with each other

--- 24_goweb/simple_server.go
+++ 24_goweb/simple_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,24 +30,52 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+var manager = map[string]*websocket.Conn{}
+
+type IMMessage struct {
+	Message string `json:"message"`
+	From    string `json:"from"`
+	To      string `json:"to"`
+}
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	user := r.Header.Get("User")
+	fmt.Println(user, "connect to websocket successfully...")
+
 	conn, err := upgrader.Upgrade(w, r, nil)
+
+	manager[user] = conn
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	for k, v := range manager {
+		if k != user {
+			if err := v.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%s is online now.", user))); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+
 	for {
-		messageType, p, err := conn.ReadMessage()
+		messageType, jsonMsg, err := manager[user].ReadMessage()
+		msg := IMMessage{}
+		json.Unmarshal(jsonMsg, &msg)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(messageType, string(p))
+		fmt.Println(string(jsonMsg))
 
-		if err := conn.WriteMessage(messageType, []byte("hello from server")); err != nil {
-			fmt.Println(err)
-			return
+		if msg.To != "" {
+			err := manager[msg.To].WriteMessage(messageType, []byte(msg.Message))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 		}
 	}
 }
