fc8c202ca1bdbaf06c38c60995f9c775a9d50959
zzy <15779712170@163.com>
2021-12-03 08:35:03 +0800

    chore(websocket): implements ws on http api

--- 24_goweb/simple_server.go
+++ 24_goweb/simple_server.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"strconv"
 	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 var counter int
@@ -22,6 +24,33 @@ func incrementCounter(w http.ResponseWriter, r *http.Request) {
 	mutex.Unlock()
 }
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(messageType, string(p))
+
+		if err := conn.WriteMessage(messageType, []byte("hello from server")); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
 func main() {
 
 	http.HandleFunc("/", echoString)
@@ -31,6 +60,8 @@ func main() {
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hi")
 	})
+
+	http.HandleFunc("/ws", wsEndpoint)
 
 	// listen and server on localhost:8081
 	log.Fatal(http.ListenAndServe(":8081", nil))
--- go.mod
+++ go.mod
@@ -5,8 +5,13 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-sql-driver/mysql v1.6.0
+<<<<<<< HEAD
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/iamdavidzeng/gonameko v0.0.2-beta
+=======
+	github.com/gorilla/websocket v1.4.2
+	github.com/iamdavidzeng/gonamekoclient v0.0.1-beta
+>>>>>>> ae88bfc... chore(websocket): implements ws on http api
 	github.com/jinzhu/gorm v1.9.16
 	github.com/kr/text v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
