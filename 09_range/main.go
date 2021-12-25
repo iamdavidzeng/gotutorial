0094c758ea6c2c8b70ff8bc83ff5903c32f9cad2
iamdavidzeng <iamdavidzeng@163.com>
2021-12-25 15:52:17 +0800

    feat(pointer): check if is palindrome link table

--- 09_range/main.go
+++ 09_range/main.go
@@ -2,6 +2,46 @@ package main
 
 import "fmt"
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func traverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	// 前序遍历
+	fmt.Println("link table before:", head.Val)
+
+	traverseList(head.Next)
+
+	// 后续遍历
+	fmt.Println("link table after:", head.Val)
+
+	return nil
+}
+
+func traverseTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	// 前序
+	traverseTree(root.Left)
+	// 中序
+	traverseTree(root.Right)
+	// 后序
+	fmt.Println("tree after:", root.Val)
+
+	return nil
+}
+
 func main() {
 	// string
 	str := "david"
@@ -30,4 +70,31 @@ func main() {
 	for k, v := range emails {
 		fmt.Println(k, v)
 	}
+
+	// link table iteration
+	linkTable := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val:  2,
+			Next: nil,
+		},
+	}
+	for p := linkTable; p != nil; p = p.Next {
+		fmt.Println(p.Val)
+	}
+
+	// link table recursion
+	traverseList(linkTable)
+
+	// binary tree recursion
+	tree := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}
+	traverseTree(tree)
 }
---
+++ 22_algorithm/pointer/reverse_table.go
@@ -0,0 +1,30 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	last := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
+
+func main() {
+	list1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val:  3,
+				Next: nil,
+			},
+		},
+	}
+	reverseList(list1)
+}
